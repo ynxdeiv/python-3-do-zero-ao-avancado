e6faad107f71ebd809d0b8ca77daab0177e09d22
deiv <deivid652015@gmail.com>
2025-04-02 07:28:38 -0300

    Exercício para validar CPF

---
+++ aula61.py
@@ -0,0 +1,21 @@
+'''
+Cálculo Primeiro Dígito CPF
+
+Verificar se é válido.
+
+'''
+
+novo_cpf = ''
+
+while True:
+
+    cpf_inserido = input('Digite seu CPF: ')
+    
+    for digito in cpf_inserido:
+        if digito.isdigit():
+            novo_cpf += digito
+
+    print(novo_cpf)
+
+
+    ...
