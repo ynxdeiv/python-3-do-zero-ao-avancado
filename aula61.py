7225ece709158e80e58e607019d6338011526807
deiv <deivid652015@gmail.com>
2025-04-02 12:28:49 -0300

    Código finalizado, porém acredito que tem como melhorar com as boas práticas da programação

--- aula61.py
+++ aula61.py
@@ -23,7 +23,6 @@ while True:
         nove_digitos_str = str(nove_digitos)
 
 
-
         for contador in range (10, 1, -1):
             
             digito_convertido = int(nove_digitos_str[i])
@@ -38,7 +37,27 @@ while True:
 
         print(f'O valor do primeiro dígito é {calculo_resultado}')
 
-        
+        dez_digitos = nove_digitos_str + str(calculo_resultado)
+
+        i=0
+        calculo_soma_digitos = 0 
+
+        for contador in range (11, 1, -1):
+            
+            digito_convertido = int(dez_digitos[i])
+
+            calculo_soma_digitos += digito_convertido*contador
+
+            i+=1
+
+        calculo_resultado = (calculo_soma_digitos*10)%11
+
+        calculo_resultado = 0 if calculo_resultado >= 10 else calculo_resultado
+
+        print(f'O valor do segundo dígito é {calculo_resultado}')
+
+
+
         
     except ValueError:
         print('NÃO PODE SER UM CPF')
